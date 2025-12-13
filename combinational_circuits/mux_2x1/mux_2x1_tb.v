b645d55a0bc3a44726631731f6398adbf3dde43f
Snehal-ReddyP <saisnehal.mitmpl2024@learner.manipal.edu>
2025-12-13 20:07:32 +0530

    mux_2x1_tb.v

---
+++ combinational_circuits/mux_2x1/mux_2x1_tb.v
@@ -0,0 +1,20 @@
+`timescale 1ns/1ps
+
+module mux_2x1_tb;
+    reg a, b, sel;
+    wire y;
+
+    mux_2x1 dut (.a(a), .b(b), .sel(sel), .y(y));
+
+    initial begin
+        $dumpfile("mux_2x1.vcd");
+        $dumpvars(0, mux_2x1_tb);
+
+        a=0; b=1; sel=0; #10;
+        a=0; b=1; sel=1; #10;
+        a=1; b=0; sel=0; #10;
+        a=1; b=0; sel=1; #10;
+
+        #10 $finish;
+    end
+endmodule
