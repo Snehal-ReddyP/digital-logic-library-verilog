29f15e2aa18017bbe5e10a9c4ce232118c3b8604
Snehal-ReddyP <saisnehal.mitmpl2024@learner.manipal.edu>
2025-12-13 19:59:17 +0530

    full_adder_tb.v

---
+++ combinational_circuits/full_adder/full_adder_tb.v
@@ -0,0 +1,24 @@
+`timescale 1ns/1ps
+
+module full_adder_tb;
+    reg a, b, cin;
+    wire sum, carry;
+
+    full_adder dut (.a(a), .b(b), .cin(cin), .sum(sum), .carry(carry));
+
+    initial begin
+        $dumpfile("full_adder.vcd");
+        $dumpvars(0, full_adder_tb);
+
+        a=0; b=0; cin=0; #10;
+        a=0; b=0; cin=1; #10;
+        a=0; b=1; cin=0; #10;
+        a=0; b=1; cin=1; #10;
+        a=1; b=0; cin=0; #10;
+        a=1; b=0; cin=1; #10;
+        a=1; b=1; cin=0; #10;
+        a=1; b=1; cin=1; #10;
+
+        #10 $finish;
+    end
+endmodule
